c144a94bf5f830372f0eea3286b6e11394b6c40d
Miguel Duque Filho <miguel.duque@globo.com>
2020-06-05 16:30:42 -0300

    carga do Backend, Frontend e mobile da semana OmniStack #11

---
+++ backend/src/database/connection.js
@@ -0,0 +1,10 @@
+const Knex = require("knex");
+const configuration = require("../../knexfile");
+
+const config =
+  process.env.NODE_ENV === "test"
+    ? configuration.test
+    : configuration.development;
+const connection = Knex(config);
+
+module.exports = connection;
